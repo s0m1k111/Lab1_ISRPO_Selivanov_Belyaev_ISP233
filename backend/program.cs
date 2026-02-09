f9ec1254eff22080d899dbe67a85e2481b2e1986
s0m1k111 <spavel1207@gmail.com>
2026-02-09 16:09:22 +0300

    name

--- backend/program.cs
+++ backend/program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Привет, {name}!");
+
+string[] days = { "Воскресенье", "Понедельник", "Вторник", "Среда","Четверг", "Пятница", "Суббота" };
+Console.WriteLine($"Сегодня {days[(int)DateTime.Now.DayOfWeek]}");
+
+Console.Write("Хотите продолжить? (y/n): ");
+string answer = Console.ReadLine();
+
+if (answer == "y" || answer == "Y")
+{
+    Console.WriteLine("Продолжаем работу...");
+}
+else
+{
+    Console.WriteLine("Выход из программы.");
+}
